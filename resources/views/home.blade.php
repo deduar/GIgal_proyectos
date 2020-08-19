332674da5f49ff468d99ec841b84890db8f4563a
Eduardo Diez <deduar@gmail.com>
2020-08-19 15:27:08 +0200

    Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,32 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    {{ __('You are logged in!') }} 
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="card card-stats">
+                                <div class="card-header card-header-warning card-header-icon">
+                                    <div class="card-icon">
+                                        <i class="material-icons">content_copy</i>
+                                    </div>
+                                    <p class="card-category">Used Space</p>
+                                    <h3 class="card-title">49/50
+                                        <small>GB</small>
+                                    </h3>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="stats">
+                                        <i class="material-icons text-danger">warning</i>
+                                        <a href="#pablo">Get More Space...</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
